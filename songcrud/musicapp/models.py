516fa3f11a30159d8171efb5430301514be674ba
Spiritbck <spiritbck@gmail.com>
2022-10-29 01:33:43 -0700

    Simple CRUD app in models.py

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,28 @@
+from ast import Delete
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    # Declearing database fields
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+
+
+class Song(models.Model):
+    # many-to-one relation
+    Artist = models.ForeignKey(Artist, on_delete=models.CASCADE ) 
+    # Declearing database fields
+    title = models.CharField(max_length=30)
+    artist_id = models.AutoField(primary_key=True)
+    likes = models.IntegerField(default=0)
+    date_released = models.DateTimeField('date released')
+
+
+class Lyrics(models.Model):
+    content = models.CharField(max_length=1000)
+    song_id = models.AutoField(primary_key=True)
