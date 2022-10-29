4a8db6ed6f393c97c80ebd7749a2dd09db4b6ea4
Spiritbck <spiritbck@gmail.com>
2022-10-29 02:41:35 -0700

    Simple CRUD

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Artist, Lyrics, Song
 
 # Register your models here.
+
+admin.site.register(Artist)
+admin.site.register(Lyrics)
+admin.site.register(Song)
