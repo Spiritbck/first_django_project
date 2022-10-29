7ca33f31ad2fef6296aa4c0136ed6d5937911566
Spiritbck <spiritbck@gmail.com>
2022-10-29 02:43:46 -0700

    Simple Crud

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -12,17 +12,21 @@ class Artist(models.Model):
     age = models.IntegerField()
 
 
+class Lyrics(models.Model):
+    # many-to-one relation
+    Artist = models.ForeignKey(Artist, on_delete=models.CASCADE ) 
+
+    content = models.CharField(max_length=1000)
+    song_id = models.AutoField(primary_key=True)
+
 
 class Song(models.Model):
     # many-to-one relation
     Artist = models.ForeignKey(Artist, on_delete=models.CASCADE ) 
+
+    
     # Declearing database fields
     title = models.CharField(max_length=30)
     artist_id = models.AutoField(primary_key=True)
     likes = models.IntegerField(default=0)
     date_released = models.DateTimeField('date released')
-
-
-class Lyrics(models.Model):
-    content = models.CharField(max_length=1000)
-    song_id = models.AutoField(primary_key=True)
